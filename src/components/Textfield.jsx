ac68ab6230eabc51f1e9b8be5690a41ad3531c41
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-30 11:25:54 +0800

    added textfield,jsx

---
+++ src/components/Textfield.jsx
@@ -0,0 +1,17 @@
+export default function Textfield({
+  type = "text",
+  padding = "p-3",
+  bgcolor = "bg-BGPrimary",
+  textcolor = "text-TextPrimary",
+  onClick,
+  placeholder,
+}) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={`${padding} ${bgcolor} ${padding} ${textcolor} bg-white first-line:text-sm text bg-white w-full rounded-lg shadow-md text-black max-w-md focus:outline-BtnPrimary-start `}
+      onClick={onClick}
+    />
+  );
+}
