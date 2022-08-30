43abb977c92b9898b0c5908d5de62ffaceeb61b5
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-30 10:31:35 +0800

    added Routing for Client Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import LandingPage from "./Client/pages/LandingPage";
+
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/signup" element={<LandingPage/>}/>
+          <Route path="/login" element={<LandingPage/>}/>
+          <Route path="/properties" element={<LandingPage/>}/>
+          <Route path="/about" element={<LandingPage/>}/>
+          <Route path="/property" element={<LandingPage/>}/>
+          <Route path="/agents" element={<LandingPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
