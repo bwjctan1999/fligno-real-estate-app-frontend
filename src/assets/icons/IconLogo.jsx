627b756904eb21d171329e814d44f4da469ad13f
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-30 14:00:27 +0800

    added IconLogo.jsx

---
+++ src/assets/icons/IconLogo.jsx
@@ -0,0 +1,117 @@
+export default function IconLogo() {
+  return (
+    <svg
+      width="33"
+      height="33"
+      viewBox="0 0 33 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_232_2952)">
+        <rect
+          width="33"
+          height="33"
+          rx="8"
+          fill="url(#paint0_linear_232_2952)"
+        />
+        <path
+          d="M-1.87646 14.5936C-1.87646 14.2277 -1.67658 13.891 -1.35532 13.7157L8.26285 8.46946C8.74093 8.2087 9.33977 8.3784 9.60995 8.85122L24.2686 34.5039C24.6495 35.1705 24.1682 36 23.4004 36H-0.876465C-1.42875 36 -1.87646 35.5523 -1.87646 35V14.5936Z"
+          fill="white"
+        />
+        <rect
+          x="-8"
+          y="21.6935"
+          width="18.1963"
+          height="4"
+          rx="1"
+          transform="rotate(-27.99 -8 21.6935)"
+          fill="#396AFC"
+        />
+        <rect
+          x="1.12354"
+          y="39.5398"
+          width="18.1963"
+          height="4"
+          rx="1"
+          transform="rotate(-27.99 1.12354 39.5398)"
+          fill="#396AFC"
+        />
+        <rect
+          x="-3.87646"
+          y="27.0944"
+          width="17.2471"
+          height="4"
+          rx="1"
+          transform="rotate(-27.99 -3.87646 27.0944)"
+          fill="#396AFC"
+        />
+        <rect
+          x="-0.876465"
+          y="33.0944"
+          width="17.2471"
+          height="4"
+          rx="1"
+          transform="rotate(-27.99 -0.876465 33.0944)"
+          fill="#396AFC"
+        />
+        <rect
+          x="19"
+          y="9.38489"
+          width="20"
+          height="33.9321"
+          rx="1"
+          transform="rotate(-27.9852 19 9.38489)"
+          fill="white"
+        />
+        <rect
+          x="23"
+          y="12.0944"
+          width="17.2471"
+          height="4"
+          transform="rotate(-27.99 23 12.0944)"
+          fill="#396AFC"
+        />
+        <rect
+          x="26"
+          y="17.0944"
+          width="17.2471"
+          height="4"
+          transform="rotate(-27.99 26 17.0944)"
+          fill="#396AFC"
+        />
+        <rect
+          x="29"
+          y="22.0944"
+          width="17.2471"
+          height="4"
+          transform="rotate(-27.99 29 22.0944)"
+          fill="#396AFC"
+        />
+        <rect
+          x="32"
+          y="28.0944"
+          width="17.2471"
+          height="4"
+          transform="rotate(-27.99 32 28.0944)"
+          fill="#396AFC"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_232_2952"
+          x1="0"
+          y1="16.5"
+          x2="33"
+          y2="16.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#396AFC" />
+          <stop offset="1" stop-color="#2948FF" />
+        </linearGradient>
+        <clipPath id="clip0_232_2952">
+          <rect width="33" height="33" rx="8" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
