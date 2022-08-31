14c21080981ea3f8dce4f74c75ffeb9f9e6716bb
Crstinemaearatan <20190017152@my.xu.edu.ph>
2022-08-31 09:01:42 +0800

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LandingPage from "./Client/pages/LandingPage";
+import Login from "./Client/pages/Login";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
           <Route path="/signup" element={<LandingPage/>}/>
-          <Route path="/login" element={<LandingPage/>}/>
+          <Route path="/login" element={<Login />}/>
           <Route path="/properties" element={<LandingPage/>}/>
           <Route path="/about" element={<LandingPage/>}/>
           <Route path="/property" element={<LandingPage/>}/>
