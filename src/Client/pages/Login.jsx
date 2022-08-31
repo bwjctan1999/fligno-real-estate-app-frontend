9801e82a4d2d9045abee9d5b608f4e8ed487d4c1
Crstinemaearatan <20190017152@my.xu.edu.ph>
2022-08-31 16:27:31 +0800

    Created Login UI

--- src/Client/pages/Login.jsx
+++ src/Client/pages/Login.jsx
@@ -1,62 +1,45 @@
 import DesignLogin from "../../assets/svgs/DesignLogin";
-import Button from "../../components/Button"
+import Button from "../../components/Button";
 import TextField from "../../components/Textfield";
 
-
 export default function Login() {
-    return (
-        <div className="flex flex-wrap gap-9 min-h-screen items-center justify-center bg-gray-50">
-          <div className=" px-12 pt-12 pb-10 text-left md:w-5/12 lg:w-5/12 xl:w-4/12 ">
-            
-            <h3 className="mb-2 pb-3 text-4xl font-bold text-zinc-900 ">
-            Login
-            </h3>
+  return (
+    <div className="flex flex-row flex-wrap-reverse  gap-y-16  h-screen">
+      <div className="flex flex-col mt-40 mx-auto">
+        <h1 className="mb-2 pb-3 font-bold text-3xl text-TextTertiary">
+          Log In
+        </h1>
 
-            <p className="text-sm mb-4">
-            Not registered? 
-            <a  
-                onClick={() => navigate("/Signup")} 
-                className="text-blue-600 hover:underline">
+        <p className="flex gap-x-3 text-sm mb-4 text-TextTertiary">
+          Not Registered?
+          <a
+            onClick={() => navigate("/Login")}
+            className="text-BtnPrimary-start hover:underline"
+          >
             Sign Up
-            </a>
-            </p>
+          </a>
+        </p>
+        <div className="flex flex-col gap-y-3 w-auto ">
+          <TextField type="text" placeholder="Email" />
 
-        <form action="">
-         
+          <TextField type="Password" placeholder="Password" />
 
-        <div className= "flex flex-col gap-4">
-            <TextField
-                type="text"
-                placeholder="Email"
-            />
-
-            <TextField
-                type="Password"
-                placeholder="Password"
-            />
-
-        <div >
-          <input type="radio" />
-          <label class="text-sm">     Remember Me</label>
-            <a  
-                onClick={() => navigate("/Signup")} 
-                className="text-sm text-blue-600 hover:underline">
-                Forgot Password?
-            </a>
-        </div>
-
-        <div className="flex items-baseline justify-between ">
-            <Button text="Log In"/>
+          <div className="flex flex-wrap gap-x-4  gap-y-3">
+            <input type="checkbox" />
+            <label class="text-sm "> Remember Me</label>
+            <div className="flex flex-row gap-x-12 gap-y-3">   
+              <a class="text-sm text-BtnPrimary-start hover:underline "> Forgot Password?</a>
+            </div>
           </div>
 
+          <div className="text-sm mt-7">
+            <Button text="Log In" />
+          </div>
         </div>
-        </form>
-        </div>
-
-        <div className="w-1/2 items-end">
-          <DesignLogin />
-        </div>
-        </div>
-    );
+      </div>
+      <div className="justify-center items-center w-6/12  bg-BGSecondary hidden lg:flex">
+        <DesignLogin />
+      </div>
+    </div>
+  );
 }
-
