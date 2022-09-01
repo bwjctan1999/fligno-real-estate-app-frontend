574c47b00e03da9a4f77069c812160265ea1161a
Crstinemaearatan <20190017152@my.xu.edu.ph>
2022-09-01 09:36:31 +0800

    Create ClientDashboard ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import ClientDashboard from "./Client/pages/ClientDashboard";
 import LandingPage from "./Client/pages/LandingPage";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
           <Route path="/" element={<LandingPage/>}/>
           <Route path="/signup" element={<LandingPage/>}/>
           <Route path="/login" element={<LandingPage/>}/>
+          <Route path="/dashboard" element={<ClientDashboard/>}/>
           <Route path="/properties" element={<LandingPage/>}/>
           <Route path="/about" element={<LandingPage/>}/>
           <Route path="/property" element={<LandingPage/>}/>
---
+++ src/Client/pages/ClientDashboard.jsx
@@ -0,0 +1,15 @@
+
+
+export default function ClientDashboard() {
+    return(
+
+        <div className="flex flex-row flex-wrap-reverse  gap-y-16  h-screen">
+            <h1 className="mb-2 px-32 pt-36 font-bold text-3xl text-TextSecondary">
+                 Dashboard
+                </h1>
+        </div>
+
+       
+
+    );
+}
