1b9ea3da81cd713a1d9fbda023946e6c81f1159e
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-02 15:56:58 +0800

    fixed responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage/>}/>
-          <Route path="/signup" element={<LandingPage/>}/>
-          <Route path="/login" element={<LandingPage/>}/>
-          <Route path="/dashboard" element={<ClientDashboard/>}/>
-          <Route path="/properties" element={<LandingPage/>}/>
-          <Route path="/about" element={<LandingPage/>}/>
-          <Route path="/property" element={<LandingPage/>}/>
-          <Route path="/agents" element={<LandingPage/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<LandingPage />} />
+          <Route path="/login" element={<LandingPage />} />
+          <Route path="/dashboard" element={<ClientDashboard />} />
+          <Route path="/properties" element={<LandingPage />} />
+          <Route path="/about" element={<LandingPage />} />
+          <Route path="/property" element={<LandingPage />} />
+          <Route path="/agents" element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
