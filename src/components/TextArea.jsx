62afcc55f85ecddcdb40f0de65df5011cdcdcb63
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-05 09:36:28 +0800

    changed TextArea max width

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,19 +1,12 @@
-export default function TextArea({
-   
-  }) {
-    return (
-    <div className="relative w-full lg:max-w-2xl ">
-
-        <label 
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-TextPrimary"></label>
-
-
-        <textarea rows="6" 
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-md  focus:outline-BtnPrimary-start" placeholder="Description"></textarea>
-        
-        
-        </div>
-        );
-  }
-  
-  
+export default function TextArea({}) {
+  return (
+    <div className="relative w-full">
+      <label className="text-gray-900 mb-2 block text-sm font-medium dark:text-TextPrimary"></label>
+      <textarea
+        rows="6"
+        className="text-gray-900 bg-gray-50 block w-full rounded-lg p-2.5 text-sm shadow-md  focus:outline-BtnPrimary-start"
+        placeholder="Description"
+      ></textarea>
+    </div>
+  );
+}
