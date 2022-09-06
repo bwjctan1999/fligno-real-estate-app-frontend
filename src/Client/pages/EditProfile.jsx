f9920e9adb9846390442d7f04835a0cdad59adff
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-06 14:54:58 +0800

    refactored editprofile

--- src/Client/pages/EditProfile.jsx
+++ src/Client/pages/EditProfile.jsx
@@ -4,41 +4,29 @@ import Button from "../../components/Button";
 
 export default function EditProfile() {
   return (
-    <div className="min-h-screen bg-BGSecondary">
-      <div className="h-full p-4 lg:p-20">
-        <h1 className="mb-10 text-4xl font-bold text-TextTertiary">
-          Add Property
-        </h1>
-        <div className="flex flex-col gap-5 lg:flex-row lg:gap-20  ">
-          <div className="flex h-full flex-col justify-center gap-4 lg:w-2/5 ">
-            <label className="font-black text-TextTertiary" for="price">
-              Add Photo
-            </label>
-
-            <div className="h-full w-full">
-              <ImageUploader />
-            </div>
-          </div>
-
-          <div className="flex w-full flex-col gap-4 lg:w-1/2 lg:px-10">
-            <label className="font-black text-TextTertiary" for="price">
-              Information
-            </label>
-            <Textfield id="email" placeholder="Email" type="Email" />
-            <Textfield id="pass" placeholder="Password" type="Password" />
-            <Textfield
-              id="conpass"
-              placeholder="Confirm Password"
-              type="Password"
-            />
-            <Textfield id="fname" placeholder="Full Name" type="name" />
-            <Textfield id="mobnum" placeholder="Mobile Number" type="" />
-          </div>
+    <div className="h-screen bg-BGSecondary p-4 lg:p-20">
+      <h1 className="mb-10 text-4xl font-bold text-TextTertiary">
+        Add Property
+      </h1>
+      <div className="flex w-full flex-col gap-5 lg:flex-row lg:gap-20">
+        <div className="flex flex-col gap-5 lg:w-1/2">
+          <p className="font-black text-TextTertiary">Information</p>
+          <ImageUploader />
         </div>
-        <div className="float-right ">
-          <Button custom={"w-64"} text="Save" />
+
+        <div className="flex flex-col lg:w-1/2 lg:px-10">
+          <div className="flex flex-col gap-y-5">
+            <p className="font-black text-TextTertiary">Information</p>
+            <Textfield placeholder="Email" type="email" />
+            <Textfield placeholder="Password" type="password" />
+            <Textfield placeholder="Confirm Password" type="password" />
+            <Textfield placeholder="Full Name" />
+            <Textfield placeholder="Mobile Number" />
+          </div>
         </div>
       </div>
+
+      <Button text="Save" custom="float-right w-full lg:w-72 m-10" />
     </div>
   );
 }
