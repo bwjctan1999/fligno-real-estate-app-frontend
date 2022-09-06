87e1387afb9a40abc45d9e52037f8134590dff83
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-06 12:27:20 +0800

    added min screen to clients page

--- src/Client/pages/Clients.jsx
+++ src/Client/pages/Clients.jsx
@@ -2,7 +2,7 @@ import TableMaker from "../../components/TableMaker";
 
 export default function Clients() {
   return (
-    <div className="h-screen w-screen bg-BGSecondary lg:px-60">
+    <div className="min-h-screen bg-BGSecondary lg:px-60 lg:pt-32">
       <TableMaker />
     </div>
   );
