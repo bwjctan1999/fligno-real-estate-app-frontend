40cd3134bd383b5e1331747dc197469017d2c4ee
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-06 11:33:13 +0800

    added routing for clients

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PaymentMethod from "./Client/pages/PaymentMethod";
 import PropertyList from "./Client/pages/PropertyList";
 import SignUp from "./Client/pages/SignUp";
 import Subscription from "./Client/pages/Subscription";
+import Clients from "./Client/pages/Clients";
 
 function App() {
   return (
@@ -20,12 +21,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<ClientDashboard />} />
           <Route path="/subscription" element={<Subscription />} />
-          <Route path="/waiting-payment" element={<PaymentMethod/>}/>
+          <Route path="/waiting-payment" element={<PaymentMethod />} />
           <Route path="/properties" element={<PropertyList />} />
           <Route path="/about" element={<LandingPage />} />
-          <Route path="/property" element={<PropertyDetails/>} />
-          <Route path="/clients" element={<LandingPage />} />
-          <Route path="/add-property" element={<AddProperty/>}/>
+          <Route path="/property" element={<PropertyDetails />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/add-property" element={<AddProperty />} />
         </Routes>
       </BrowserRouter>
     </div>
