d00d3dfa9e7ec19dfd958959cc5ca9e69f0490d4
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-06 11:33:37 +0800

    added clients page

---
+++ src/Client/pages/Clients.jsx
@@ -0,0 +1,9 @@
+import TableMaker from "../../components/TableMaker";
+
+export default function Clients() {
+  return (
+    <div className="h-screen w-screen bg-BGSecondary lg:px-60">
+      <TableMaker />
+    </div>
+  );
+}
