cc32bf096e049d8b282faabbc7272f202da1bb41
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-08 14:37:24 +0800

    refactored TextArea

--- src/components/general/TextArea.jsx
+++ src/components/general/TextArea.jsx
@@ -1,10 +1,11 @@
-export default function TextArea({
-placeholder = "",
-}) {
+export default function TextArea({ value, onChange, placeholder = "", id }) {
   return (
     <div className="relative w-full">
       <label className="text-gray-900 mb-2 block text-sm font-medium dark:text-TextPrimary"></label>
       <textarea
+        id={id}
+        value={value}
+        onChange={onChange}
         rows="6"
         className="text-gray-900 bg-gray-50 block w-full rounded-lg p-2.5 text-sm shadow-md  focus:outline-BtnPrimary-start"
         placeholder={placeholder}
