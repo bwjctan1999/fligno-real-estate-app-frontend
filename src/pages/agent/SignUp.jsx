50912fe39bd4b79f9b8553934f1c46527bd2ae5f
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-08 14:05:47 +0800

    refactored states and added html validation

--- src/pages/agent/SignUp.jsx
+++ src/pages/agent/SignUp.jsx
@@ -6,20 +6,42 @@ import axios from "axios";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [first_name, setFirstName] = useState("");
-  const [last_name, setLastName] = useState("");
-  const [p_number, setNum] = useState("");
-  const [user_type, setUserType] = useState("Client");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    p_number: "",
+    user_type: "",
+  });
 
-  const PostData = () => {
-    axios.post("http://localhost:8000/api/register", {
-      email,
-      password,
-      full_name,
-      p_number,
-      user_type,
-    });
+  const setValue = (e, name) => {
+    setFormValues((oldValues) => ({
+      ...oldValues,
+      [name]: e.target.value,
+    }));
+  };
+
+  const saveFormData = async () => {
+    await axios
+      .post("http://localhost:8000/api/register", formValues)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(esponse);
+      });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await saveFormData();
+      alert("Check your email for confirmation");
+    } catch (e) {
+      alert(`Sign Up Failed! ${e.message}`);
+    }
   };
 
   return (
@@ -38,68 +60,78 @@ export default function SignUp() {
             Login
           </a>
         </p>
-        <div className="flex w-auto flex-col gap-y-3 md:w-96 ">
-          <TextField
-            type="email"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
 
-          <TextField
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <form onSubmit={onSubmit}>
+          <div className="flex w-auto flex-col gap-y-3 md:w-96 ">
+            <TextField
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setValue(e, "email")}
+              required="true"
+            />
 
-          <TextField
-            type="password"
-            placeholder="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
+            <TextField
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setValue(e, "password")}
+              required="true"
+            />
 
-          <TextField
-            type="text"
-            placeholder="First Name"
-            onChange={(e) => setFullName(e.target.value)}
-          />
+            <TextField
+              type="password"
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required="true"
+            />
 
-          <TextField
-            type="text"
-            placeholder="Last Name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
+            <TextField
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => setValue(e, "first_name")}
+              required="true"
+            />
 
-          <TextField
-            type="text"
-            placeholder="Mobile Number"
-            onChange={(e) => setLastName(e.target.value)}
-          />
+            <TextField
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => setValue(e, "last_name")}
+              required="true"
+            />
 
-          <div className="flex flex-wrap gap-x-16  gap-y-3">
-            <label> Sign up as:</label>
-            <div className="flex flex-row gap-x-4 gap-y-3">
-              <input
-                type="radio"
-                name="radio"
-                value="client"
-                onChange={(e) => setUserType(e.target.value)}
-              />
-              <label> Client</label>
+            <TextField
+              type="text"
+              placeholder="Mobile Number"
+              onChange={(e) => setValue(e, "p_number")}
+              required="true"
+            />
 
-              <input
-                type="radio"
-                name="radio"
-                value="agent"
-                onChange={(e) => setUserType(e.target.value)}
-              />
-              <label> Agent</label>
+            <div className="flex flex-wrap gap-x-16  gap-y-3">
+              <label> Sign up as:</label>
+              <div className="flex flex-row gap-x-4 gap-y-3">
+                <input
+                  type="radio"
+                  name="radio"
+                  value="client"
+                  onChange={(e) => setValue(e, "user_type")}
+                  required="true"
+                />
+                <label> Client</label>
+
+                <input
+                  type="radio"
+                  name="radio"
+                  value="agent"
+                  onChange={(e) => setValue(e, "user_type")}
+                />
+                <label> Agent</label>
+              </div>
+            </div>
+
+            <div className="mt-7 text-sm">
+              <Button type="submit" text="Sign Up" />
             </div>
           </div>
-
-          <div className="mt-7 text-sm">
-            <Button text="Sign Up" onClick={PostData} />
-          </div>
-        </div>
+        </form>
       </div>
       <div className="hidden h-screen w-6/12  items-center justify-center bg-BtnPrimary-end lg:flex">
         <DesignSignUp />
