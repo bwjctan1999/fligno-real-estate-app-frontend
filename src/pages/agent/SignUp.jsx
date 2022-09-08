35e1b31c51a9aba87733f146e4ad2c70e5ebc2ed
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-08 12:20:52 +0800

    made radio buttons work

--- src/pages/agent/SignUp.jsx
+++ src/pages/agent/SignUp.jsx
@@ -7,9 +7,10 @@ import axios from "axios";
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [full_name, setFullName] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
   const [p_number, setNum] = useState("");
-  const [user_type, setAsType] = useState("Client");
+  const [user_type, setUserType] = useState("Client");
 
   const PostData = () => {
     axios.post("http://localhost:8000/api/register", {
@@ -22,13 +23,13 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex h-screen flex-row  flex-wrap-reverse  items-center gap-y-16">
-      <div className="mx-auto mt-14 flex flex-col">
+    <div className="flex h-screen flex-row flex-wrap-reverse items-center gap-y-16">
+      <div className="mx-auto flex flex-col">
         <h1 className="mb-2 pb-3 text-3xl font-bold text-TextTertiary">
           Sign Up
         </h1>
 
-        <p className="mb-4 flex gap-x-3 text-sm text-TextTertiary">
+        <p className="mb-4 flex gap-x-3 text-TextTertiary">
           Already A Member?
           <a
             onClick={() => navigate("/Login")}
@@ -58,24 +59,40 @@ export default function SignUp() {
 
           <TextField
             type="text"
-            placeholder="Full Name"
+            placeholder="First Name"
             onChange={(e) => setFullName(e.target.value)}
           />
 
           <TextField
             type="text"
+            placeholder="Last Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+
+          <TextField
+            type="text"
             placeholder="Mobile Number"
-            onChange={(e) => setNum(e.target.value)}
+            onChange={(e) => setLastName(e.target.value)}
           />
 
           <div className="flex flex-wrap gap-x-16  gap-y-3">
-            <label class="text-sm "> Sign up as:</label>
+            <label> Sign up as:</label>
             <div className="flex flex-row gap-x-4 gap-y-3">
-              <input type="radio" name="radio" />
-              <label class="text-sm"> Client</label>
+              <input
+                type="radio"
+                name="radio"
+                value="client"
+                onChange={(e) => setUserType(e.target.value)}
+              />
+              <label> Client</label>
 
-              <input type="radio" name="radio" />
-              <label class="text-sm"> Agent</label>
+              <input
+                type="radio"
+                name="radio"
+                value="agent"
+                onChange={(e) => setUserType(e.target.value)}
+              />
+              <label> Agent</label>
             </div>
           </div>
 
