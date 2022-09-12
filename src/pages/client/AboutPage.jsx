d99a1545f72eb768fa094f4fe20155bb8194aa00
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-12 10:33:24 +0800

    fixed responsiveness and sizing

--- src/pages/client/AboutPage.jsx
+++ src/pages/client/AboutPage.jsx
@@ -1,54 +1,54 @@
 import DesignAboutPage from "../../assets/svgs/DesignAboutPage";
-import BuyProperty from "../../assets/imgs/BuyProperty.png";
-import RentProperty from "../../assets/imgs/RentProperty.png";
+import DesignBuyProperty from "../../assets/svgs/DesignBuyProperty";
+import DesignRentProperty from "../../assets/svgs/DesignRentProperty";
 
 export default function AboutPage() {
   return (
     <div className="w-full">
-      <div className="flex flex-col justify-center">
-        <div className="flex h-1/4 w-full items-center justify-center gap-20 bg-BGSecondary  md:flex ">
-          <div className="flex flex-col lg:w-2/4 ">
-            <h1 className="mb-5 flex text-4xl font-bold text-TextTertiary">
-              About Real Estate
-            </h1>
-            <p className="text-justify leading-9">
-              In search of a home to purchase or rent? You're covered by us!
-              Your one-stop shop for all rental homes and real estate listings.
-            </p>
-          </div>
-          <div className="hidden md:h-1/3 md:w-1/3  lg:flex">
-            <DesignAboutPage />
-          </div>
+      <div className="flex w-full flex-col-reverse items-center justify-center gap-20 bg-BGSecondary py-20 px-4 md:px-24 lg:flex-row file:lg:px-32">
+        <div className="flex flex-col gap-4 lg:w-1/2">
+          <h1 className="text-Primary flex text-3xl font-bold lg:text-5xl">
+            About Real Estate
+          </h1>
+          <p className="text-justify text-xl font-medium leading-10 text-TextTertiary">
+            In search of a home to purchase or rent? You're covered by us! Your
+            one-stop shop for all rental homes and real estate listings.
+          </p>
         </div>
+        <div className="w-full lg:flex lg:w-1/2">
+          <DesignAboutPage />
+        </div>
+      </div>
 
-        <div className="flex flex-col items-center py-20 md:flex">
-          <p>Our Services</p>
-          <h1 className="mb-8 flex text-4xl font-bold text-TextTertiary">
-            Services That We Provide{" "}
+      <div className="flex min-h-screen flex-col gap-10 py-16 px-4 md:px-24 lg:px-32">
+        <div className="flex flex-col items-center text-center">
+          <p className="text-2xl font-medium">Our Services</p>
+          <h1 className="flex text-4xl font-bold text-TextPrimary">
+            Services That We Provide
           </h1>
         </div>
-
-        <div className="flex flex-wrap items-center justify-center gap-y-10 sm:flex-wrap md:flex-wrap lg:flex-row">
-          <div className="flex w-1/2 flex-col items-center">
-            <div className="mb-10 h-5/6">
-              <img src={BuyProperty} />
+        <div className="flex flex-col items-center justify-center gap-x-20 lg:flex-row">
+          <div className="flex flex-col items-center gap-4 lg:w-1/2">
+            <div className="w-full lg:p-20">
+              <DesignBuyProperty />
             </div>
-            <h1 className="text-xl font-bold text-TextTertiary">
+            <h1 className="text-2xl font-bold text-TextTertiary">
               Buy Property
             </h1>
-            <p className=" text-center">
+            <p className=" text-center font-medium text-TextSecondary">
               Provides the chance to find the greatest home sale choices.
             </p>
           </div>
 
-          <div className="flex w-1/2 flex-col items-center">
-            <div className="mb-10 h-5/6">
-              <img src={RentProperty} />
+          <div className="flex flex-col items-center gap-4 lg:w-1/2">
+            <div className="w-full lg:p-20">
+              <DesignRentProperty />
             </div>
-            <h1 className="text-xl font-bold text-TextTertiary">
+
+            <h1 className="text-2xl font-bold text-TextTertiary">
               Rent Property
             </h1>
-            <p className=" text-center">
+            <p className="text-center font-medium text-TextSecondary">
               Provides listing for condominiums and houses that are available
               for rent.
             </p>
