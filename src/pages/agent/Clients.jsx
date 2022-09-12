438d6d3dd8b33fd6307f5cefe0667355c19be21f
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-12 13:56:12 +0800

    added navbar to clients page

--- src/pages/agent/Clients.jsx
+++ src/pages/agent/Clients.jsx
@@ -1,8 +1,10 @@
 import TableMaker from "../../components/general/TableMaker";
+import AgentNavbar from "../../components/agent/AgentNavbar";
 
 export default function Clients() {
   return (
-    <div className="min-h-screen bg-BGSecondary lg:px-60 lg:pt-32">
+    <div className=" bg-BGSecondary pt-16 lg:px-60 lg:pt-32">
+      <AgentNavbar />
       <TableMaker />
     </div>
   );
