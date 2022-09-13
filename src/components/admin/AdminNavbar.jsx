763717ccc5dd70f187564def5d265a33c05e0d1d
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-13 09:10:04 +0800

    created AdminNavbar

---
+++ src/components/admin/AdminNavbar.jsx
@@ -0,0 +1,82 @@
+import IconLogo from "../../assets/icons/IconLogo";
+import IconDashboard from "../../assets/icons/IconDashboard";
+import IconUsersList from "../../assets/icons/IconUsersList";
+import IconProperties from "../../assets/icons/IconProperties";
+import IconAdd from "../../assets/icons/IconAdd";
+import IconNotification from "../../assets/icons/IconNotification";
+import DesignProfile from "../../assets/svgs/DesignProfile.svg";
+import IconMenu from "../../assets/icons/IconMenu";
+import IconKey from "../../assets/icons/IconKey";
+
+import { useState } from "react";
+import { useEffect } from "react";
+import Button from "../general/Button";
+
+export default function AdminNavbar({}) {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      window.innerWidth > 768 ? setOpenNavbar(true) : null;
+    }
+
+    function handleLoad() {
+      window.innerWidth > 768 ? setOpenNavbar(true) : setOpenNavbar(false);
+    }
+
+    window.addEventListener("load", handleLoad);
+    window.addEventListener("resize", handleResize);
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() => setOpenNavbar(true)}
+        className="fixed left-0 top-0 p-4 lg:hidden"
+      >
+        <IconMenu width="30" height="30" />
+      </button>
+      <nav
+        className={`${
+          openNavbar === true ? "" : "-translate-x-full"
+        } fixed top-0 left-0 z-10 h-full w-3/4 items-center bg-BGPrimary p-4 shadow-lg duration-300 ease-out lg:h-auto lg:w-full lg:flex-row`}
+      >
+        <div className="flex h-full w-full flex-col justify-between lg:flex-row">
+          <div className="flex gap-4">
+            <button onClick={() => setOpenNavbar(false)} className="lg:hidden">
+              <IconMenu width="30" height="30" />
+            </button>
+            <IconLogo />
+            <label className="text-2xl font-black text-BtnPrimary-end">
+              Turnkey
+            </label>
+          </div>
+          <ul className="flex flex-col gap-10 text-xl font-black text-TextTertiary lg:flex-row lg:items-center">
+            <li className="flex h-full items-center gap-2">
+              <IconDashboard />
+              <a> Dashboard</a>
+            </li>
+            <li className="flex h-full items-center gap-2 fill-TextTertiary">
+              <IconProperties width="26" height="26" fill="fill-TextTertiary" />
+              <a> Properties</a>
+            </li>
+            <li className="flex h-full items-center gap-2">
+              <IconUsersList width="26" height="26" fill="fill-TextTertiary" />
+              <a> Users</a>
+            </li>
+            <li className="flex h-full items-center gap-2">
+              <IconKey />
+              <a> Roles</a>
+            </li>
+          </ul>
+          <div className="flex items-center gap-4 ">
+            <IconNotification />
+            <div className="w-24">
+              <Button text="Logout" fontsize="text-basic" padding="p-2" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
