3889bf042ccaa8bcbd3f72e0b88c5130c2381b4e
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-13 10:00:53 +0800

    rearrange imports and added AdmiRoles route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AdminPropertyDetails from "../src/pages/admin/AdminPropertyDetails";
 import AddProperty from "./pages/agent/AddProperty";
 import AgentDashboard from "./pages/agent/AgentDashboard";
@@ -12,9 +11,11 @@ import Subscription from "./pages/agent/Subscription";
 import EditProfile from "./pages/agent/EditProfile";
 import Clients from "./pages/agent/Clients";
 import AdminDashboard from "./pages/admin/AdminDashboard";
-import routes from "./routes";
 import AboutPage from "./pages/client/AboutPage";
-import AgentPropertyDetails from "./pages/agent/AgentPropertyDetails";
+import AdminRoles from "./pages/admin/AdminRoles";
+
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
   return (
@@ -28,17 +29,14 @@ function App() {
           <Route path="/subscription" element={<Subscription />} />
           <Route path="/waiting-payment" element={<PaymentMethod />} />
           <Route path="/properties" element={<PropertyList />} />
-          <Route path="/about" element={<AboutPage/>} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/property" element={<PropertyDetails />} />
           <Route path="/clients" element={<Clients />} />
           <Route path="/add-property" element={<AddProperty />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/admin-property" element={<AdminPropertyDetails />} />
-          <Route path="/agent-property" element={<AgentPropertyDetails />} />
-          
-    
-       
+          <Route path="/roles" element={<AdminRoles />} />
         </Routes>
       </BrowserRouter>
     </div>
