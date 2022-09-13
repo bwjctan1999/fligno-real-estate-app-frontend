7ec90af571d246f91bf20b5f50f29d9021dc3d78
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-13 10:01:42 +0800

    refactored IconUser by adding props

--- src/assets/icons/IconUser.jsx
+++ src/assets/icons/IconUser.jsx
@@ -1,6 +1,8 @@
-export default function IconUser() {
+export default function IconUser({ width = "24", height = "24" }) {
   return (
     <svg
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -18,5 +20,3 @@ export default function IconUser() {
     </svg>
   );
 }
- 
-
