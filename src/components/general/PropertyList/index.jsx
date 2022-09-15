76137f45f60e13c08e10d4e86009681c730bee86
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-15 14:52:48 +0800

    fixed key requirements

--- src/components/general/PropertyList/index.jsx
+++ src/components/general/PropertyList/index.jsx
@@ -2,7 +2,7 @@ import PropertyCard from "./PropertyCard";
 import PropertyFilter from "./PropertyFilter";
 
 export default function PropertyList() {
-  const addCard = () => {
+  const addCard = (key) => {
     return (
       <PropertyCard
         title="Elite Garden Residence"
@@ -11,6 +11,7 @@ export default function PropertyList() {
         bednum={5}
         bathnum={2}
         price={1000000}
+        key={key}
       />
     );
   };
@@ -19,7 +20,7 @@ export default function PropertyList() {
     <div className="flex flex-col gap-8 bg-BGSecondary">
       <PropertyFilter />
       <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-4">
-        {[...Array(25)].map((x, i) => addCard())}
+        {[...Array(25)].map((x, i) => addCard(i))}
       </div>
     </div>
   );
