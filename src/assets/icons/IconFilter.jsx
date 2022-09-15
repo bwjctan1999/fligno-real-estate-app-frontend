e4d34622115e861b46640d6a8d1605256667eb85
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-15 14:50:59 +0800

    fixed syntax errors

--- src/assets/icons/IconFilter.jsx
+++ src/assets/icons/IconFilter.jsx
@@ -12,10 +12,9 @@ export default function IconFilter({
       viewBox="0 0 24 24"
       fill={fill}
       stroke={stroke}
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-filter"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
     </svg>
