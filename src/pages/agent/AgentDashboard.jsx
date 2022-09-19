032308e0ec52569a47bc72db92098283d9ed8957
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-19 11:08:53 +0800

    added width and height to Icons

--- src/pages/agent/AgentDashboard.jsx
+++ src/pages/agent/AgentDashboard.jsx
@@ -4,28 +4,30 @@ import DashboardCard from "../../components/general/DashboardCard";
 
 export default function AgentDashboard() {
   return (
-    <div className="w-screen bg-BGSecondary p-2 pt-16 md:h-screen md:px-20 md:pt-28">
-      <div className="pb-7 text-4xl font-bold text-TextSecondary">
-        Dashboard
-      </div>
+    <div className="w-screen bg-BGSecondary p-2 pt-16 md:h-screen md:px-20 md:pt-28 ">
+      <div className="pb-7 text-4xl font-bold">Dashboard</div>
 
       <div className=" grid w-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
         <DashboardCard
           title="Properties"
           num="9"
-          icon={<IconProperties fill="fill-TextTertiary" />}
+          icon={<IconProperties width="100%" height="100%" />}
         />
         <DashboardCard
           title="Rent"
           num="9"
-          icon={<IconProperties fill="fill-TextTertiary" />}
+          icon={<IconProperties width="100%" height="100%" />}
         />
         <DashboardCard
           title="Reserved"
           num="9"
-          icon={<IconProperties fill="fill-TextTertiary" />}
+          icon={<IconProperties width="100%" height="100%" />}
         />
-        <DashboardCard title="Reserved" num="9" icon={<IconUsersList />} />
+        <DashboardCard
+          title="Reserved"
+          num="9"
+          icon={<IconUsersList width="100%" height="100%" />}
+        />
       </div>
     </div>
   );
