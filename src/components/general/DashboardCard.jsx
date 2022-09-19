6ad474a0b6824693cc24f2f2f34f563c54fcac9e
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-19 11:08:39 +0800

    fixed icons too smal in dashboard card

--- src/components/general/DashboardCard.jsx
+++ src/components/general/DashboardCard.jsx
@@ -1,15 +1,11 @@
 export default function DashboardCard({ title, num, icon }) {
   return (
-    <div className="border-1-8 border-transparent flex items-center justify-between space-y-2 rounded-md bg-BGPrimary shadow-md">
+    <div className="border-1-8 border-transparent flex items-center justify-between space-y-2 rounded-md bg-BGPrimary text-TextTertiary shadow-md">
       <div className="w-1/2">
-        <h2 className="text-center text-5xl font-bold text-TextSecondary">
-          {num}
-        </h2>
-        <p className="text-center text-lg font-light text-TextSecondary">
-          {title}
-        </p>
+        <h2 className="text-center text-5xl font-bold">{num}</h2>
+        <p className="text-center text-lg font-light">{title}</p>
       </div>
-      <div className="w-1/2 p-10">{icon}</div>
+      <div className="w-1/2 p-8">{icon}</div>
     </div>
   );
 }
