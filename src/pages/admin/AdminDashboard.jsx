d94e5bcb06e859738e250905c2f30d790e91dbc3
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-19 11:12:56 +0800

    fixed icon too small in admin dashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -6,13 +6,19 @@ import PopUpReason from "../../components/general/PopUpReason";
 export default function AdminDashboard() {
   return (
     <div className="w-screen bg-BGSecondary p-4 pt-16 md:h-screen md:p-24">
-      <div className="pb-7 text-4xl font-bold text-TextSecondary">
-        Dashboard
-      </div>
+      <div className="pb-7 text-4xl font-bold">Dashboard</div>
 
       <div className="grid w-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
-        <DashboardCard title="Properties" num="19" icon={<IconProperties />} />
-        <DashboardCard title="Users" num="5" icon={<IconUsersList />} />
+        <DashboardCard
+          title="Properties"
+          num="19"
+          icon={<IconProperties width="100%" height="100%" />}
+        />
+        <DashboardCard
+          title="Users"
+          num="5"
+          icon={<IconUsersList width="100%" height="100%" />}
+        />
       </div>
     </div>
   );
