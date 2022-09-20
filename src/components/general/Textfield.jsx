55a202e3193cc1378a7a776ad09283ebcdc71718
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-20 12:37:23 +0800

    added validation to textfield

--- src/components/general/Textfield.jsx
+++ src/components/general/Textfield.jsx
@@ -7,23 +7,36 @@ export default function Textfield({
   onClick,
   placeholder,
   onChange,
-  required,
+  valid = true,
+  invalidError = "required",
 }) {
+  const validationBorderHandler = (validator) => {
+    if (validator === false)
+      return " border-2 border-solid border-r-BtnTertiary-end";
+  };
+
   return (
-    <div
-      className={` ${bgcolor} ${textcolor} ${
-        icon ? "pl-3" : null
-      } flex items-center rounded-lg shadow-md outline-2 outline-BtnPrimary-start focus-within:outline`}
-    >
-      {icon}
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={`${padding} h-full w-full rounded-lg outline-none`}
-        onClick={onClick}
-        onChange={onChange}
-        required={required}
-      />
+    <div>
+      <div
+        className={`
+          ${bgcolor} 
+          ${textcolor} 
+          ${icon ? "pl-3" : null} 
+          ${valid ? null : "border-2 border-BtnTertiary-end"} 
+          flex flex-col items-center rounded-lg shadow-md outline-2 outline-BtnPrimary-start focus-within:outline`}
+      >
+        {icon}
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={`${padding} h-full w-full rounded-lg outline-none`}
+          onClick={onClick}
+          onChange={onChange}
+        />
+      </div>
+      {valid ? null : (
+        <p className="text-sm text-BtnTertiary-end">{invalidError}</p>
+      )}
     </div>
   );
 }
