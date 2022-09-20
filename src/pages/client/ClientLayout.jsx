7547e4a507089094d0efbc1dbc9e1774bf43a995
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-20 09:00:47 +0800

    added footer to clientlayout

--- src/pages/client/ClientLayout.jsx
+++ src/pages/client/ClientLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 
+import Footer from "../../components/client/Footer";
+
 export default function ClientLayout() {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   );
 }
