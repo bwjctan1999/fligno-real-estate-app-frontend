3fe75769f6e9f2a9fe0425624705776a8f146db8
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-20 12:42:42 +0800

    added validation to Dropdown

--- src/components/general/DropDown.jsx
+++ src/components/general/DropDown.jsx
@@ -1,17 +1,30 @@
-export default function DropDown({ value, onChange, options = [] }) {
+export default function DropDown({
+  value,
+  onChange,
+  valid = false,
+  invalidError = "required",
+  options = [],
+}) {
   return (
-    <div className="relative w-full ">
-      <select
-        value={value}
-        onChange={onChange}
-        className="bg-white text bg-white w-full rounded-lg p-3 text-base text-TextPrimary shadow-md focus:outline-BtnPrimary-start"
-      >
-        {options.map((option) => (
-          <option className="leading-3" key={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div>
+      <div className="relative w-full ">
+        <select
+          value={value}
+          onChange={onChange}
+          className={`
+          ${valid ? null : "border-2 border-BtnTertiary-end"} 
+          bg-white text bg-white w-full rounded-lg p-3 text-base text-TextPrimary shadow-md focus:outline-BtnPrimary-start`}
+        >
+          {options.map((option) => (
+            <option className="leading-3" key={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+      {valid ? null : (
+        <p className="text-sm text-BtnTertiary-end">{invalidError}</p>
+      )}
     </div>
   );
 }
