2d25f7600059627dfd41316751ca677a74a9d513
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-20 08:51:34 +0800

    added responsiveness to page 404

--- src/pages/general/FourZeroFour.jsx
+++ src/pages/general/FourZeroFour.jsx
@@ -1,14 +1,14 @@
-import DesignError from "../../assets/svgs/DesignError.svg";
+import DesignPageNotFound from "../../assets/svgs/DesignPageNotFound";
 
 export default function FourZeroFour() {
   return (
     <div className=" flex min-h-screen flex-col-reverse justify-evenly bg-BGSecondary px-4 md:p-16 lg:flex-row lg:px-32">
-      <div className=" flex h-3/5 items-start lg:h-auto lg:items-center selection:lg:w-1/2">
-        <div className="flex flex-col gap-2 lg:gap-4 ">
-          <h1 className="text-3xl font-extrabold text-TextPrimary lg:text-6xl ">
+      <div className=" flex h-3/5 items-start lg:h-auto lg:w-1/2 lg:items-center">
+        <div className="flex flex-col gap-2 lg:gap-4 lg:pl-20">
+          <h1 className="text-center text-9xl font-extrabold text-TextPrimary">
             404
           </h1>
-          <p className="pt-1 font-medium leading-10 text-TextPrimary">
+          <p className="pt-1 text-justify text-xl font-medium leading-10 text-TextPrimary">
             The page you are looking for was moved, removed, renamed or might
             never existed. You have found yourself in our little cabin instead.
             Unfortunately, it isn’t for sale.
@@ -16,8 +16,8 @@ export default function FourZeroFour() {
         </div>
       </div>
 
-      <div className="lg:w-4/ mx-10 flex h-2/5 items-center justify-center lg:h-auto">
-        <img src={DesignError} className="max-w-xs" />
+      <div className="flex w-full items-center justify-center md:px-20 lg:h-auto lg:w-1/2 lg:p-28">
+        <DesignPageNotFound />
       </div>
     </div>
   );
