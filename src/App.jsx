b4754f8ce3898f66c777a51bfe64b86e0950aed9
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-21 13:39:51 +0800

    removed old routes and replaced with useRoute

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ import Subscription from "./pages/client/Subscription";
 import SignUp from "./pages/client/SignUp";
 import PaymentMethod from "./pages/client/PaymentMethod";
 
-import routes from "./routes";
 import FourZeroFour from "./pages/general/FourZeroFour";
+import RouteList from "./routes";
 
 function App() {
   const [user, setUser] = useState({ user: null, userType: null });
@@ -37,51 +37,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ClientLayout />}>
-            <Route index element={<LandingPage />} />
-            <Route path="*" element={<FourZeroFour />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route path="login" element={<Login setUser={setUser} />} />
-            <Route path="subscription" element={<Subscription />} />
-            <Route path="waiting-payment" element={<PaymentMethod />} />
-            <Route path="properties" element={<Properties />} />
-            <Route path="agents" element={<Agents />} />
-            <Route path="about" element={<AboutPage />} />
-
-            <Route path="property" element={<PropertyDetails />} />
-            <Route path="property/:propertyId" element={<PropertyDetails />} />
-          </Route>
-
-          <Route path="/agent" element={<AgentLayout />}>
-            <Route index element={<AgentDashboard />} />
-            <Route path="clients" element={<Clients />} />
-            <Route path="add-property" element={<AddProperty />} />
-            <Route path="properties" element={<AgentProperties />} />
-            <Route path="property" element={<AgentPropertyDetails />} />
-            <Route path="edit-property" element={<EditProperty />} />
-            <Route
-              path="edit-property/:propertyId"
-              element={<EditProperty />}
-            />
-          </Route>
-
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="roles" element={<AdminRoles />} />
-            <Route path="properties" element={<AdminProperties />} />
-            <Route path="users" element={<AdminUsersList />} />
-
-            <Route path="edit-role" element={<AdminEditRole />} />
-            <Route path="edit-role/:roleId" element={<AdminEditRole />} />
-
-            <Route path="property" element={<AdminPropertyDetails />} />
-            <Route
-              path="property/:propertyId"
-              element={<AdminPropertyDetails />}
-            />
-          </Route>
-        </Routes>
+        <RouteList />
       </BrowserRouter>
     </div>
   );
