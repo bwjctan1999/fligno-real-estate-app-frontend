08cf14bf58a7a676ab36db46275e3c66da0af8dd
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-26 10:56:51 +0800

    renamed function

--- src/api/ApiEmail.js
+++ src/api/ApiEmail.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const base_url = "http://127.0.0.1:8000/api/verify";
 
-export async function PostProperty(data) {
+export async function PostEmail(data) {
   const resolved = {
     response: null,
     error: null,
