ba9db6618e418fffd78f210784d7f38629dee862
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-28 11:01:31 +0800

    changed margin

--- src/pages/admin/AdminProperties.jsx
+++ src/pages/admin/AdminProperties.jsx
@@ -1,12 +1,16 @@
 import PropertyList from "../../components/general/PropertyList";
+import PropertyFilter from "../../components/general/PropertyList/PropertyFilter";
 
 export default function AdminProperties() {
   return (
     <div className="min-h-screen bg-BGSecondary px-4 pt-24 lg:px-24">
-      <div className="mb-10 text-4xl font-bold text-TextTertiary">
+      <div className="mb-5 text-4xl font-bold text-TextTertiary">
         Properties
       </div>
-      <PropertyList />
+      <div className="flex w-full flex-col items-end">
+        <PropertyFilter />
+        <PropertyList />
+      </div>
     </div>
   );
 }
