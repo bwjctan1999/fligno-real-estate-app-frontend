5d684e16b0d8d216f5480c15c967996efc803a51
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-28 11:02:13 +0800

    integrated filter and search button changes

--- src/pages/agent/AgentProperties.jsx
+++ src/pages/agent/AgentProperties.jsx
@@ -1,19 +1,27 @@
 import PropertyList from "../../components/general/PropertyList";
+import PropertyFilter from "../../components/general/PropertyList/PropertyFilter";
 import Button from "../../components/general/Button";
 import IconAdd from "../../assets/icons/IconAdd";
+import { useNavigate } from "react-router-dom";
 
 export default function AgentProperties() {
+  const navigate = useNavigate();
+
   return (
     <div className="min-h-screen bg-BGSecondary px-4 pt-24 lg:px-24">
       <div className="mb-10 text-4xl font-bold text-TextTertiary">
         Properties
       </div>
-      <div className="ml-auto w-full md:w-1/5">
+      <div className="flex justify-end gap-4">
         <Button
           text="Add Property"
           icon={<IconAdd />}
           onClick={() => navigate("/agent/add-property")}
+          custom="md:w-1/5 w-1/4"
+          responsive="true"
+          padding="p-2"
         />
+        <PropertyFilter />
       </div>
       <PropertyList url="http://localhost:8000/api/property" />
     </div>
