91a4877d82fd2b2f9de7b2ab56f28c4b1072f0cd
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-28 11:01:16 +0800

    cleaned

--- src/components/general/PropertyList/PropertyFilter.jsx
+++ src/components/general/PropertyList/PropertyFilter.jsx
@@ -3,13 +3,8 @@ import Textfield from "../../../components/general/Textfield";
 
 export default function PropertyFilter() {
   return (
-<div className="flex lg:justify-end md:">
-          <div className="lg:w-1/4 md:w-1/3 w-full" >
-           
-            <Textfield placeholder="Search" icon={<IconSearch />} />
-      
-          </div>
-      
+    <div className="float-right w-full md:w-1/3 lg:w-1/4">
+      <Textfield placeholder="Search" icon={<IconSearch />} />
     </div>
   );
 }
