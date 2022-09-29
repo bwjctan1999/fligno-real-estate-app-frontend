4b5067370b40a677372d58cee2a5fb96040113e2
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-29 09:22:17 +0800

    refactored

--- src/pages/client/VerifyEmail.jsx
+++ src/pages/client/VerifyEmail.jsx
@@ -1,18 +1,29 @@
-
 export default function VerifyEmail() {
   return (
-      <div class="flex items-center justify-center h-screen">
+    <div className="flex h-screen items-center justify-center bg-BGSecondary">
       <div>
-        <div class="flex flex-col items-center space-y-4 ">
-          <svg xmlns="http://www.w3.org/2000/svg" class="text-BtnQuanary-end w-28 h-28" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        <div className="flex flex-col items-center space-y-4 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-32 w-32 text-BtnQuanary-end"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
           </svg>
-          <h1 class="text-4xl text-TextTertiary font-bold">Thank You !</h1>
-          <p>Thank you for your interest! Check your email for a link to the guide.</p>
-        
+          <h1 className="text-5xl font-bold text-TextTertiary">Thank You !</h1>
+          <p className="text-xl">
+            Thank you for your interest! please check your email for a link to
+            verify your account.
+          </p>
         </div>
       </div>
     </div>
-    
   );
 }
