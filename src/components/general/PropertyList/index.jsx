880e33a81119ed6723bfdac2ddd3a5bc802fed95
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-09-30 07:53:11 +0800

    integrated pagination component to propertylist

--- src/components/general/PropertyList/index.jsx
+++ src/components/general/PropertyList/index.jsx
@@ -3,21 +3,38 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import PropertyCard from "./PropertyCard";
+import Paginator from "../Paginator";
 
 export default function PropertyList({ url }) {
   const [properties, setProperties] = useState([]);
+  const [paginationData, setPaginationData] = useState({
+    current_page: 1,
+    last_page: null,
+    first_page_url: null,
+    last_page_url: null,
+    next_page_url: null,
+    prev_page_url: null,
+  });
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    getData();
+    getData("");
   }, []);
 
-  const getData = async () => {
-    const api_request = await GetProperty("");
+  const getData = async (url) => {
+    const api_request = await GetProperty(url);
 
     if (!api_request.error) {
       setProperties(api_request.response.data.data.data);
-      console.log(api_request.response.data.data.data);
+      setPaginationData({
+        current_page: api_request.response.data.data.current_page,
+        last_page: api_request.response.data.data.last_page,
+        first_page_url: api_request.response.data.data.first_page_url,
+        last_page_url: api_request.response.data.data.last_page_url,
+        next_page_url: api_request.response.data.data.next_page_url,
+        prev_page_url: api_request.response.data.data.prev_page_url,
+      });
     } else {
       console.log(api_request.error);
     }
@@ -55,10 +72,22 @@ export default function PropertyList({ url }) {
   };
 
   return (
-    <div className="flex flex-col gap-3">
-      <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-4">
+    <div className="flex flex-col items-center gap-3">
+      <div
+        className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-4"
+        id="client_properties"
+      >
         {properties.map((property, i) => addProperty(property))}
       </div>
+      <Paginator
+        changePage={getData}
+        current={paginationData.current_page}
+        last={paginationData.last_page}
+        start_url={paginationData.first_page_url}
+        last_url={paginationData.last_page_url}
+        next_url={paginationData.next_page_url}
+        prev_url={paginationData.prev_page_url}
+      />
     </div>
   );
 }
