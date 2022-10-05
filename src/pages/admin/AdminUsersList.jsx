a3987c785855427e41bf78557066a0065f0d61cd
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-05 09:10:53 +0800

    connected userslist to backend

--- src/pages/admin/AdminUsersList.jsx
+++ src/pages/admin/AdminUsersList.jsx
@@ -1,24 +1,72 @@
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+
+import { GetUser } from "../../api/ApiUsers";
 
 import Button from "../../components/general/Button";
 import Textfield from "../../components/general/Textfield";
+import Dropdown from "../../components/general/DropDown";
 
 import IconEdit from "../../assets/icons/IconEdit";
 import IconSearch from "../../assets/icons/IconSearch";
-import Dropdown from "../../components/general/DropDown";
+
+import { useEffect, useState } from "react";
+import Paginator from "../../components/general/Paginator";
 
 export default function AdminUsersList() {
-  const fillTable = () => {
+  const [users, setUsers] = useState([]);
+  const [paginationData, setPaginationData] = useState({
+    current_page: 1,
+    last_page: null,
+    first_page_url: null,
+    last_page_url: null,
+    next_page_url: null,
+    prev_page_url: null,
+  });
+
+  useEffect(() => {
+    getData("");
+  }, []);
+
+  const getData = async (url) => {
+    setUsers([]);
+    const api_request = await GetUser(url);
+
+    if (!api_request.error) {
+      console.log(api_request.response)
+      setUsers(api_request.response.data.data);
+      setPaginationData({
+        current_page: api_request.response.data.current_page,
+        last_page: api_request.response.data.last_page,
+        first_page_url: api_request.response.data.first_page_url,
+        last_page_url: api_request.response.data.last_page_url,
+        next_page_url: api_request.response.data.next_page_url,
+        prev_page_url: api_request.response.data.prev_page_url,
+      });
+      console.log(api_request.response.data.data);
+    } else {
+      console.log(api_request);
+    }
+  };
+
+  const addUser = ({
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    user_type,
+  }) => {
     return (
       <Tr className="border-y-2 border-LinePrimary text-TextTertiary">
-        <Td className="p-4">John J. Doe</Td>
-        <Td>johndoe@gmail.com</Td>
-        <Td>09090900909</Td>
+        <Td className="p-4">{`${first_name} ${last_name}`}</Td>
+        <Td>{email}</Td>
+        <Td>{phone_number}</Td>
         <Td>
-          <Dropdown values={[1, 2, 3]} options={["Admin", "Agent", "Client"]} />
+          <Dropdown
+            values={[2, 3]}
+            options={["Agent", "Client"]}
+            value={user_type}
+          />
         </Td>
         <Td>
           <div className="flex flex-col gap-1 lg:flex-row lg:pl-10">
@@ -66,9 +114,18 @@ export default function AdminUsersList() {
               <Th className="lg:pl-10">Actions</Th>
             </Tr>
           </Thead>
-          <Tbody>{[...Array(5)].map((x, i) => fillTable())}</Tbody>
+          <Tbody>{users.map((user, i) => addUser(user))}</Tbody>
         </Table>
       </div>
+      <Paginator
+        changePage={getData}
+        current={paginationData.current_page}
+        last={paginationData.last_page}
+        start_url={paginationData.first_page_url}
+        last_url={paginationData.last_page_url}
+        next_url={paginationData.next_page_url}
+        prev_url={paginationData.prev_page_url}
+      />
     </div>
   );
 }
