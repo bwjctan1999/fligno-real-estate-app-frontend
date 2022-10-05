4655a3049989a27522e051d1f4434680f6dd90ad
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-05 12:21:08 +0800

    changed height

--- src/components/general/TableSkeleton.jsx
+++ src/components/general/TableSkeleton.jsx
@@ -2,11 +2,11 @@ export default function TableSkeleton() {
   return (
     <div className="flex animate-pulse flex-col gap-6 rounded-lg bg-BGPrimary p-4 shadow-lg">
       <div className="grid grid-cols-3 gap-2 lg:grid-cols-5">
-        <div className="h-5 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
-        <div className="h-5 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
-        <div className="h-5 w-8/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
-        <div className="h-5 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
-        <div className="h-5 w-full rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
+        <div className="h-6 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
+        <div className="h-6 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
+        <div className="h-6 w-8/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
+        <div className="h-6 w-4/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
+        <div className="h-6 w-full rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
       </div>
       <div className="grid grid-cols-3 gap-2 lg:grid-cols-5">
         <div className="h-5 w-9/12 rounded bg-Skeleton-end dark:bg-Skeleton-start"></div>
