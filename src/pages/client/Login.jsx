13957419e7ede7709cc476f3e8650a1354b3abb5
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-10 11:37:38 +0800

    added loading

--- src/pages/client/Login.jsx
+++ src/pages/client/Login.jsx
@@ -3,37 +3,21 @@ import { useState } from "react";
 import DesignLogin from "../../assets/svgs/DesignLogin";
 import Button from "../../components/general/Button";
 import TextField from "../../components/general/Textfield";
-import axios from "axios";
+import { PostLogin } from "../../api/ApiLogin";
+import DesignSpinner from "../../assets/svgs/DesignSpinner";
 
 export default function Login({ setUser }) {
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [login, setLogin] = useState(true);
 
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    try {
-      const response = await axios.post("http://localhost:8000/api/login", {
-        email: email,
-        password: password,
-      });
-      localStorage.setItem("user_role", response.data.data.user_role);
-      localStorage.setItem("token", response.data.data.Token);
-      console.log(response);
-      switch (response.data.data.user_role) {
-        case 2:
-          navigate("/agent");
-          break;
-        case 3:
-          navigate("/");
-          break;
-        default:
-          break;
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    setLoading(true);
+    const api_request = await PostLogin(email, password);
+    console.log(api_request);
+    setLoading(false);
   };
 
   return (
@@ -65,7 +49,7 @@ export default function Login({ setUser }) {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          <div className="flex flex-wrap gap-x-4  gap-y-3">
+          <div className="flex flex-wrap gap-x-4 gap-y-3">
             <input type="checkbox" />
             <label className="text-sm "> Remember Me</label>
             <div className="flex flex-row gap-x-12 gap-y-3">
@@ -76,7 +60,18 @@ export default function Login({ setUser }) {
           </div>
 
           <div className="mt-7 text-sm">
-            <Button text="Log In" onClick={handleLogin} />
+            {loading ? (
+              <div className="flex justify-center rounded-lg bg-BtnPrimary-end p-2">
+                <DesignSpinner
+                  width="30"
+                  height="30"
+                  bgcolor="text-TextOnDark"
+                  color="fill-BtnPrimary-end"
+                />
+              </div>
+            ) : (
+              <Button text="Log In" onClick={handleLogin} />
+            )}
           </div>
         </div>
       </div>
