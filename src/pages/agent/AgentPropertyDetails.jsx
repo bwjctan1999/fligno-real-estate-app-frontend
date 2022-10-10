090909e4ffae86949aa1cb4bf26d86de7d53f4b9
Cristinemaearatan <20190017152@my.xu.edu.ph>
2022-10-10 11:04:20 +0800

    connected agent edit property

--- src/pages/agent/AgentPropertyDetails.jsx
+++ src/pages/agent/AgentPropertyDetails.jsx
@@ -1,10 +1,13 @@
 import Button from "../../components/general/Button";
 import IconEdit from "../../assets/icons/IconEdit";
 import IconRemove from "../../assets/icons/IconRemove";
+import { useNavigate } from "react-router-dom";
 
 import PropertyDetails from "../../components/general/PropertyDetails";
 
 export default function AgentPropertyDetails() {
+  const navigate = useNavigate();
+
   return (
     <div className="item-center flex h-auto flex-wrap gap-y-10 bg-BGSecondary py-20 px-4 lg:px-20">
       <div className=" grid w-full grid-cols-3 gap-2 lg:w-2/5">
@@ -16,6 +19,7 @@ export default function AgentPropertyDetails() {
           bgcolor="none"
           textcolor="text-BtnPrimary-end"
           custom="border-2 border-BtnPrimary-end "
+          onClick={() => navigate("/agent/edit-property/:propertyid")}
           responsive="true"
           fontsize="text-sm"
           padding="p-2"
