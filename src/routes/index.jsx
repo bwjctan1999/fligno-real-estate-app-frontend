4f3a4d3f12bc0aa52601f944f612b2498f6d2c16
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-11 14:02:14 +0800

    added protected routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -152,12 +152,12 @@ export default function RouteList() {
     //Admin Routes
     {
       path: "/admin",
-      element: <AdminLayout />,
-        // localStorage.getItem("user_role") != 1 ? (
-        //   <Navigate to="/" />
-        // ) : (
-        //   <AdminLayout />
-        // ),
+      element:
+        localStorage.getItem("user_role") != 1 ? (
+          <Navigate to="/" />
+        ) : (
+          <AdminLayout />
+        ),
       children: [
         {
           index: true,
