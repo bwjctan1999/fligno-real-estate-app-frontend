52553801bfde2faac4a931a7637543bde82b8453
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-12 12:32:13 +0800

    integrated changepassword to backend

--- src/pages/general/ForgetPassPage.jsx
+++ src/pages/general/ForgetPassPage.jsx
@@ -1,18 +1,46 @@
-import DesignLogin from "../../assets/svgs/DesignLogin";
+import { useState, useEffect } from "react";
+
 import Button from "../../components/general/Button";
 import TextField from "../../components/general/Textfield";
+
+import DesignLogin from "../../assets/svgs/DesignLogin";
+import DesignSpinner from "../../assets/svgs/DesignSpinner";
+import IconSuccessful from "../../assets/icons/IconSuccessful";
+
 import { ChangePassword } from "../../api/ApiPassword";
-import { useState } from "react";
 
 export default function ForgetPassPage({}) {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [token, setToken] = useState("");
 
-  const resetPasswordHandler = () => {
-    const api_request = ChangePassword();
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-    if (!api_request) {
+  useEffect(() => {
+    const url = window.location.href.split("=");
+    const token = url[url.length - 1];
+
+    setEmail(localStorage.getItem("reset_pass_email"));
+    setToken(token);
+  }, []);
+
+  const resetPasswordHandler = async () => {
+    setLoading(true);
+    const api_request = await ChangePassword({
+      email: email,
+      password: password,
+      password_confirmation: confirmPassword,
+      token: token,
+    });
+
+    if (!api_request.error) {
+      setLoaded(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setLoading(false);
     } else {
+      console.log(api_request.error);
     }
   };
 
@@ -28,6 +56,12 @@ export default function ForgetPassPage({}) {
 
         <div className="flex w-auto flex-col gap-y-3 md:w-96 ">
           <TextField
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+
+          <TextField
             type="Password"
             placeholder="New Password"
             onChange={(e) => setPassword(e.target.value)}
@@ -42,7 +76,33 @@ export default function ForgetPassPage({}) {
           />
 
           <div className="mt-7 text-sm">
-            <Button text="Reset Password" onClick={resetPasswordHandler} />
+            {loading ? (
+              <div className="flex items-center justify-center gap-1 rounded-lg bg-gradient-to-r from-BtnPrimary-start to-BtnPrimary-end p-2">
+                {loaded ? (
+                  <div className="animate-bounce">
+                    <IconSuccessful
+                      width="32"
+                      height="32"
+                      color="text-TextOnDark"
+                    />
+                  </div>
+                ) : (
+                  <div className="flex animate-fade-in items-center gap-1">
+                    <DesignSpinner
+                      width="32"
+                      height="32"
+                      bgcolor="text-TextOnDark"
+                      color="fill-BtnPrimary-end"
+                    />
+                    <p className="font-semibold text-TextOnDark">
+                      Logging you in...
+                    </p>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Button text="Reset Password" onClick={resetPasswordHandler} />
+            )}
           </div>
         </div>
       </div>
