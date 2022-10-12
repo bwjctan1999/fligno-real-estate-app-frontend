e389a181a87ffb7b76d6352a76919b311583821b
Cristinemaearatan <20190017152@my.xu.edu.ph>
2022-10-12 14:30:53 +0800

    changed design

--- src/pages/general/ResetPassword.jsx
+++ src/pages/general/ResetPassword.jsx
@@ -1,12 +1,12 @@
-import DesignLogin from "../../assets/svgs/DesignLogin";
+import DesignForgotPass from "../../assets/svgs/DesignForgotPass";
 import Button from "../../components/general/Button";
 import TextField from "../../components/general/Textfield";
 
 export default function ResetPassword({}) {
   return (
     <div className="flex h-screen flex-row flex-wrap-reverse justify-center bg-BGSecondary">
-      <div className="ml-20 hidden w-6/12 items-center  justify-center lg:flex">
-        <DesignLogin />
+      <div className="ml-9 mt-20 hidden w-6/12 items-center  justify-center lg:flex">
+        <DesignForgotPass />
       </div>
       <div className="mx-auto flex flex-col justify-center">
         <h1 className="mb-3 pb-3 text-3xl font-bold text-TextTertiary">
