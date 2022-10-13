0881a5f1275b203437c9165f726c7945961702d6
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-13 12:40:03 +0800

    fixed bug

--- src/components/client/ClientNavbar/index.jsx
+++ src/components/client/ClientNavbar/index.jsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect } from "react";
+import { useCallback } from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -19,7 +21,7 @@ export default function ClientNavbar({}) {
     localStorage.getItem("token") !== null
       ? setLoggedIn(true)
       : setLoggedIn(false);
-  }, []);
+  });
 
   useEffect(() => {
     function handleResize() {
