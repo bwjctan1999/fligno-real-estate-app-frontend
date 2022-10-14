836a61e6e727cc17f29ae0ef753fdf38e50e5959
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-14 14:04:48 +0800

    added search

--- src/api/ApiProperty.js
+++ src/api/ApiProperty.js
@@ -2,14 +2,29 @@ import axios from "axios";
 
 const base_url = "http://127.0.0.1:8000/api/property";
 
-export async function GetProperty(id, search) {
+export async function GetProperty(id) {
   const resolved = {
     response: null,
     error: null,
   };
 
   try {
-    resolved.response = await axios.get(`${base_url}/${id}`, {
+    resolved.response = await axios.get(`${base_url}/${id}`);
+  } catch (e) {
+    resolved.error = e;
+  }
+
+  return resolved;
+}
+
+export async function SearchProperty(search) {
+  const resolved = {
+    response: null,
+    error: null,
+  };
+
+  try {
+    resolved.response = await axios.get(`${base_url}`, {
       params: {
         search: search,
       },
