a8458848e0a09d6c1bba30fdd238583a601a64d4
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-18 09:40:40 +0800

    integrated backend

--- src/pages/client/Subscription/index.jsx
+++ src/pages/client/Subscription/index.jsx
@@ -1,47 +1,51 @@
 import Button from "../../../components/general/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SubscriptionCard from "./SubscriptionCard";
 import { useNavigate } from "react-router-dom";
+import { GetSubscriptionForClient } from "../../../api/ApiSubscription";
 
 export default function Subscription() {
+  const [plans, setPlans] = useState([]);
   const navigate = useNavigate();
   const [selected, setSelected] = useState("starter");
 
-  const table = {
-    Free: 0,
-    Starter: 1.70,
-    Premium: 4.20,
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const api_request = await GetSubscriptionForClient();
+    console.log(api_request);
+
+    if (!api_request.error) {
+      console.log(api_request.response);
+      setPlans(api_request.response.data.data);
+    } else {
+      console.log(api_request.error);
+    }
+  };
+
+  const renderCard = ({ title, description, price }) => {
+    return (
+      <SubscriptionCard
+        selected={selected}
+        title={title}
+        price={price}
+        desc={description}
+        onClick={() => setSelected(title)}
+      />
+    );
   };
 
   return (
-    <div className="flex w-screen flex-col items-center justify-center gap-12  p-4 text-center md:h-screen">
+    <div className="flex w-screen flex-col items-center justify-center gap-12 p-4 pt-24 text-center md:min-h-screen">
       <h1 className="text-5xl font-bold text-TextPrimary">Subscription</h1>
       <p className="text-base text-TextTertiary">
         Take a desired plan based on the number of properties you would want to
         sell or rent out
       </p>
       <div className="grid grid-cols-1 justify-center gap-5 lg:grid-cols-3">
-        <SubscriptionCard
-          selected={selected}
-          title="Free"
-          price="0.00"
-          desc="5 maximum postings"
-          onClick={() => setSelected("Free")}
-        />
-        <SubscriptionCard
-          selected={selected}
-          title="Starter"
-          price="1.70"
-          desc="50 maximum postings"
-          onClick={() => setSelected("Starter")}
-        />
-        <SubscriptionCard
-          selected={selected}
-          title="Premium"
-          price="4.20"
-          desc="Unlimited postings"
-          onClick={() => setSelected("Premium")}
-        />
+        {plans.map((x) => renderCard(x))}
       </div>
       <Button
         text="Next"
