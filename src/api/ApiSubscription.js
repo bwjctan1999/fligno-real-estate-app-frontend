02db8d1db792b0c78bb9fcbf842549750c71fb1c
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-18 09:39:39 +0800

    added getsubscriptionforclient

--- src/api/ApiSubscription.js
+++ src/api/ApiSubscription.js
@@ -1,19 +1,38 @@
 import axios from "axios";
 
-export async function GetSubscription(data) {
-    const resolved = {
-      response: null,
-      error: null,
-    };
-  
-    try {
-      resolved.response = await axios.get(`http://127.0.0.1:8000/api/admin/subscription-list`);
-    } catch (e) {
-      resolved.error = e;
-    }
-  
-    return resolved;
+export async function GetSubscription() {
+  const resolved = {
+    response: null,
+    error: null,
+  };
+
+  try {
+    resolved.response = await axios.get(
+      `http://127.0.0.1:8000/api/admin/subscription-list`
+    );
+  } catch (e) {
+    resolved.error = e;
   }
+
+  return resolved;
+}
+
+export async function GetSubscriptionForClient() {
+  const resolved = {
+    response: null,
+    error: null,
+  };
+
+  try {
+    resolved.response = await axios.get(
+      `http://127.0.0.1:8000/api/subscription-list`
+    );
+  } catch (e) {
+    resolved.error = e;
+  }
+
+  return resolved;
+}
 
 export async function CreateSubscription(data) {
   const resolved = {
