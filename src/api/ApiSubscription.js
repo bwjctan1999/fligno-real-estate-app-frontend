103acc5dcf846dfcf6b90f4e929d9ff7acc0a10c
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-19 10:45:29 +0800

    added api

--- src/api/ApiSubscription.js
+++ src/api/ApiSubscription.js
@@ -1,19 +1,21 @@
 import axios from "axios";
 
 export async function GetSubscription(data) {
-    const resolved = {
-      response: null,
-      error: null,
-    };
-  
-    try {
-      resolved.response = await axios.get(`http://127.0.0.1:8000/api/admin/subscription-list`);
-    } catch (e) {
-      resolved.error = e;
-    }
-  
-    return resolved;
+  const resolved = {
+    response: null,
+    error: null,
+  };
+
+  try {
+    resolved.response = await axios.get(
+      `http://127.0.0.1:8000/api/admin/subscription-list`
+    );
+  } catch (e) {
+    resolved.error = e;
   }
+
+  return resolved;
+}
 
 export async function CreateSubscription(data) {
   const resolved = {
@@ -25,6 +27,23 @@ export async function CreateSubscription(data) {
     resolved.response = await axios.post(
       `http://127.0.0.1:8000/api/admin/create-subscription`,
       data
+    );
+  } catch (e) {
+    resolved.error = e;
+  }
+
+  return resolved;
+}
+
+export async function GetSubscriptionDetails() {
+  const resolved = {
+    response: null,
+    error: null,
+  };
+
+  try {
+    resolved.response = await axios.get(
+      `http://127.0.0.1:8000/api/agent/subscription-info`
     );
   } catch (e) {
     resolved.error = e;
