9f14f22d09698c232040e7cede0c8503a3cfc0d0
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-20 09:11:09 +0800

    added create account and subscribe

--- src/pages/client/PaymentMethod.jsx
+++ src/pages/client/PaymentMethod.jsx
@@ -1,14 +1,20 @@
 import IconArrowDown from "../../assets/icons/IconArrowDown";
 import DesignSpinner from "../../assets/svgs/DesignSpinner";
+import IconSuccessful from "../../assets/icons/IconSuccessful";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Button from "../../components/general/Button";
 
+import { RegisterAccount } from "../../api/ApiSignup";
 import { PostSubscription } from "../../api/ApiSubscription";
 import { ethers } from "ethers";
 
 // 0xc2b520448aCAc4fD15ab528E5602746910867D49s
 
 export default function PaymentMethod() {
+  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const userSignupData = JSON.parse(localStorage.getItem("signupData"));
   const location = useLocation();
   const price = Number(location.state);
@@ -30,8 +36,7 @@ export default function PaymentMethod() {
     await window.ethereum
       .request({ method: "eth_sendTransaction", params })
       .then((txhash) => {
-        console.log(txhash);
-        alert("Payment Complete with transaction hash:" + txhash); // Remove if success and redirect is done
+        signUpUser(txhash);
       })
       .catch((err) => {
         console.log(
@@ -41,9 +46,42 @@ export default function PaymentMethod() {
       });
   }
 
-  const subscribeUser = () => {};
+  const signUpUser = async (hash) => {
+    setLoading(true);
+    const api_request = await RegisterAccount(userSignupData);
 
-  const signUpUser = () => {};
+    if (!api_request.error) {
+      const user_id = api_request.response.data.user_id;
+
+      subscribeUser(user_id, hash);
+    } else {
+      alert(api_request.error);
+      console.log(api_request.error);
+    }
+  };
+
+  const subscribeUser = async (user_id, hash) => {
+    console.log(user_id);
+    console.log(localStorage.getItem("selectedSubscription"));
+
+    const api_request = await PostSubscription(
+      user_id,
+      localStorage.getItem("selectedSubscription"),
+      hash
+    );
+
+    setLoaded(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setLoaded(false);
+    setLoading(false);
+
+    if (!api_request.error) {
+      alert(api_request.response);
+    } else {
+      alert(api_request.error);
+      console.log(api_request.error);
+    }
+  };
 
   return (
     <div className="min-h-scree flex flex-col-reverse justify-center bg-BGPrimary lg:flex-row">
@@ -60,19 +98,43 @@ export default function PaymentMethod() {
       </div>
       <div className="text flex h-screen flex-col items-center justify-center gap-10 p-4 text-center lg:p-20">
         <h1 className=" text-5xl font-bold text-TextTertiary">
-          Payment for Ξ {price}
+          Your Balance: Ξ {price}
         </h1>
-        <Button
-          className="sendEthButton btn"
-          text="Pay Now"
-          custom="lg:w-80"
-          onClick={sendTransaction}
-        ></Button>
-        <div>
-          <DesignSpinner />
-          <span className="sr-only">Loading...</span>
-        </div>
-        <div className="flex flex-col items-center lg:hidden">
+        {loading ? (
+          <div className="flex items-center justify-center rounded-lg bg-gradient-to-r from-BtnPrimary-start to-BtnPrimary-end p-2 lg:w-80">
+            {loaded ? (
+              <div className="animate-bounce">
+                <IconSuccessful
+                  width="32"
+                  height="32"
+                  color="text-TextOnDark"
+                />
+              </div>
+            ) : (
+              <div className="flex animate-fade-in items-center gap-1">
+                <DesignSpinner
+                  width="32"
+                  height="32"
+                  bgcolor="text-TextOnDark"
+                  color="fill-BtnPrimary-end"
+                />
+                <p className="font-semibold text-TextOnDark">
+                  Creating your account...
+                </p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Button
+            className="sendEthButton btn"
+            text="Pay Now"
+            custom="lg:w-80"
+            onClick={() =>
+              signUpUser("alskjdfhaslkdfh193y31hkansdfkahsdfkshaldfhalsflsh")
+            } //sendTransaction
+          />
+        )}
+        <div className="felx-col flex items-center lg:hidden">
           <p>scroll down</p>
           <IconArrowDown width="30" height="30" />
         </div>
