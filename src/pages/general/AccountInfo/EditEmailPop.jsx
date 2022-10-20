37caf950f7c0b8bac9a2b7414b868742ccd54fe4
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-20 09:13:08 +0800

    formatted code

--- src/pages/general/AccountInfo/EditEmailPop.jsx
+++ src/pages/general/AccountInfo/EditEmailPop.jsx
@@ -8,12 +8,12 @@ export default function EditEmailPop({
   setUserEmail,
   useremail,
   setShowSuccessAlert,
-  setShowErrorAlert
+  setShowErrorAlert,
 }) {
-
   const [email, setEmail] = useState("");
 
   const UpdateEmail = async () => {
+    console.log("IN")
     const api_request = await UpdateUser({
       email: email,
     });
@@ -23,52 +23,49 @@ export default function EditEmailPop({
       setShowSuccessAlert(true);
       setShowErrorAlert(false);
     } else {
+      console.log(api_request.error);
       setShowErrorAlert(true);
       setShowSuccessAlert(false);
     }
   };
 
-  return showEditEmailPop ?(
+  return showEditEmailPop ? (
     <div className="fixed inset-0 z-20 overflow-y-auto">
       <div className="flex min-h-screen items-center bg-TextSecondary bg-opacity-30 px-4 py-8">
         <div className="relative mx-auto w-full max-w-sm rounded-md bg-TextOnDark p-8 shadow-lg">
           <div className=" sm:flex">
             <div className="w-full text-TextPrimary">
-                <div className="text-lg text-center mb-4 font-bold">
+              <div className="mb-4 text-center text-lg font-bold">
                 Edit Email
-                </div>
-              
-
-           <div className="flex flex-col gap-3 ">
-           <div className="flex flex-col gap-3">
-            <label className="font-semibold">Email</label>
-              <Textfield
-                type="email"
-                placeholder={useremail}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
               </div>
-           
-           </div>
-              
-             
+
+              <div className="flex flex-col gap-3 ">
+                <div className="flex flex-col gap-3">
+                  <label className="font-semibold">Email</label>
+                  <Textfield
+                    type="email"
+                    placeholder={useremail}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
 
               <div className="mt-8 flex items-center gap-5 ">
-                <Button    
-                 onClick={action}
-                text="Cancel"
+                <Button
+                  onClick={action}
+                  text="Cancel"
                   bgcolor="bg-BGPrimary"
                   textcolor="text-TextSecondary"
-                  custom=" shadow-border shadow-TextSecondary opacity-60" 
-                  />
-                <Button  
-                text="Save"
+                  custom=" shadow-border shadow-TextSecondary opacity-60"
+                />
+                <Button
+                  text="Save"
                   onClick={() => {
                     UpdateEmail();
                     action();
                   }}
-                    />
+                />
               </div>
             </div>
           </div>
@@ -77,4 +74,3 @@ export default function EditEmailPop({
     </div>
   ) : null;
 }
-
