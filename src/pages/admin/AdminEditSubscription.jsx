f927b294da1a1a34ebb63f5841c2565b12d2cab8
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-10-24 12:29:26 +0800

    added states to edit subscription

--- src/pages/admin/AdminEditSubscription.jsx
+++ src/pages/admin/AdminEditSubscription.jsx
@@ -1,57 +1,67 @@
 import Textfield from "../../components/general/Textfield";
 import TextArea from "../../components/general/TextArea";
 import Button from "../../components/general/Button";
+import { useEffect, useState } from "react";
 
 export default function AdminEditSubscription() {
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {}, []);
+
+  const getSubscriptionData = () => {};
+
+  const editSubscription = () => {};
+
   return (
-<div className="bg-BGSecondary">
-    <div className="flex flex-col gap-4  px-4 pt-16 md:px-20 lg:px-60 lg:pt-28">
-    <div className="flex gap-4  flex-col h-screen">
+    <div className="bg-BGSecondary">
+      <div className="flex flex-col gap-4  px-4 pt-16 md:px-20 lg:px-60 lg:pt-28">
+        <div className="flex h-screen  flex-col gap-4">
+          <h1 className="mb-6 w-full text-4xl font-bold text-TextTertiary">
+            Edit Subscription
+          </h1>
 
-   <h1 className="mb-6 w-full text-4xl font-bold text-TextTertiary">
-        Edit Subscription
-      </h1>
+          <div className="w-full lg:w-8/12">
+            <span className=" font-black text-TextTertiary ">
+              Property Title
+            </span>
+            <Textfield
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
 
-      <div className="w-full lg:w-8/12">
-              <span className=" font-black text-TextTertiary ">
-                Property Title
-              </span>
-              <Textfield
-                placeholder="Title"
-                
-              />
-            </div>
-
-            <div className="w-full lg:w-8/12">
+          <div className="w-full lg:w-8/12">
             <label className=" font-black text-TextTertiary" htmlFor="desc">
               Description
             </label>
             <TextArea
               id="desc"
               placeholder="Description"
-          
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
 
-            <div className="w-full lg:w-8/12">
-              <label className="font-black text-TextTertiary" htmlFor="price">
-                Price
-              </label>
-              <Textfield
-                id="price "
-                placeholder="₱"
-                type="number"
-               
-              />
-            </div>
-            <div className="w-full  mt-8 lg:w-1/4">
-          <Button text="Save" />
-        
-    </div>
-    
-    
+          <div className="w-full lg:w-8/12">
+            <label className="font-black text-TextTertiary" htmlFor="price">
+              Price
+            </label>
+            <Textfield
+              id="price "
+              placeholder="₱"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <div className="mt-8  w-full lg:w-1/4">
+            <Button text="Save" />
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    );
+  );
 }
