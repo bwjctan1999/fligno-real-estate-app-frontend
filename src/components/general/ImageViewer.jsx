9660949cd07dc353db6f06b37c08c80e313c8cde
Benz Tan <benzwaltertan@gmail.com>
2022-11-04 19:35:23 +0800

    added map

--- src/components/general/ImageViewer.jsx
+++ src/components/general/ImageViewer.jsx
@@ -1,7 +1,12 @@
-export default function ImageViewer() {
+export default function ImageViewer({ images = [] }) {
   return (
     <div>
       <img />
+      <div>
+        {images.map((img) => (
+          <img src={img} />
+        ))}
+      </div>
     </div>
   );
 }
